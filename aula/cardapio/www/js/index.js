1c3e67838ee16acdfbe4aefc1c7821bdc024cf06
Edson Sensato <esensato@hotmail.com>
2024-03-08 21:55:17 -0300

    08/03/2024

--- aula/cardapio/www/js/index.js
+++ aula/cardapio/www/js/index.js
@@ -1,38 +1,77 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
+//const pizzas = [{ pizza: "Surpresa", preco: "R$ 45,00", imagem: "https://www.sabornamesa.com.br/media/k2/items/cache/9189082f4804c1ab16e77d2cfe8d09d4_XL.jpg" },
+//{ pizza: "Marguerita", preco: "R$ 80,00", imagem: "https://i0.statig.com.br/bancodeimagens/3n/0t/fy/3n0tfy3kn0jv6lkafc4cyc41u.jpg" },
+//{ pizza: "Frango com Catupiry", preco: "R$ 90,00", imagem: "https://www.sabornamesa.com.br/media/k2/items/cache/ada34cd2101afafaba465aad112ee3c1_XL.jpg" }];
+
+var pizzas;
+var idPizza = 0;
+
+// Primeira função executada após o início da app
+// Código de inicialização
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    document.getElementById("botao").addEventListener("click", exibirMensagem);
-
+    document.getElementById("direita").addEventListener("click", paginar);
+    document.getElementById("esquerda").addEventListener("click", paginar);
+    document.getElementById("enviar").addEventListener("click", enviarPedido);
+    // carregar os dados do cardapio
+    carregarCardapio()
 }
 
-function exibirMensagem() {
-    //alert('Boa noite!');
-    // obtem o texto digitado
-    let texto = document.getElementById("texto").value;
-    // exibe o texto digitado dentro do div
-    document.getElementById("resultado").innerHTML = "<b>" + texto + "</b>";
+// envia os dados do pedido
+function enviarPedido() {
+
+    let enderecoPedido = document.getElementById("endereco").value;
+    let qtdePedido = document.getElementById("qtde").value;
+
+    cordova.plugin.http.setDataSerializer('json');
+    cordova.plugin.http.post('https://pedidos-pizzaria.glitch.me/', {
+        pizza: pizzas[idPizza].pizza, quantidade: qtdePedido, endereco: enderecoPedido
+    }, {}, function (response) {
+        alert(response.status);
+    }, function (response) {
+        alert(response.error);
+    });
+}
+
+// carrega os dados das pizzas do backend
+function carregarCardapio() {
+    cordova.plugin.http.get('https://pedidos-pizzaria.glitch.me/pizzas', {}, {}, function (response) {
+        // para converter a resposta em JSON
+        pizzas = JSON.parse(response.data);
+        atualizarTela();
+    }, function (response) {
+        alert(response.error);
+    });
+}
+
+// exibe na tela o nome, preço e imagem da pizza atual
+function atualizarTela() {
+    let atual = pizzas[idPizza];
+    document.getElementById("pizza").innerHTML = atual.pizza;
+    document.getElementById("preco").innerHTML = atual.preco;
+    //document.getElementById("imagem").style.backgroundImage = "url('" + atual.imagem + "')";
+    document.getElementById("imagem").style.backgroundImage = atual.imagem;
+}
+// navega entre as pizzas do cardápio
+function paginar(evt) {
+
+    document.getElementById("esquerda").style.color = 'white';
+    document.getElementById("direita").style.color = 'white';
+    // evt.target = objeto que gerou o evento
+    evt.target.style.color = 'red';
+
+    if (evt.target.id === 'direita') {
+        idPizza++;
+    } else if (evt.target.id === 'esquerda') {
+        idPizza--;
+    }
+    // evita que seja ocorra navegação fora da quantidade de pizzas
+    if (idPizza > pizzas.length) {
+        idPizza = 0;
+    } else if (idPizza < 0) {
+        idPizza = pizzas.length - 1;
+    }
+
+    atualizarTela();
 }
 
